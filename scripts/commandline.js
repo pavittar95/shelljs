9dec52867a7b5cb103297442866a53f141c4137b
Pavittar Singh <singh.pavittar95@icloud.com>
2019-10-05 00:26:26 +0530

    git shell added

---
+++ scripts/commandline.js
@@ -0,0 +1,71 @@
+const shell = require('shelljs');
+var readline = require('readline');
+
+const createTerminalStream = () =>
+  readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+  });
+
+const readFromTerminal = callback => {
+  const rl = createTerminalStream();
+  rl.on('line', cmd => {
+    callback(cmd);
+    rl.close();
+  });
+};
+
+const gitAdd = () => {
+  shell.exec('git add -A', { slient: true }, (code, stdout, stderr) => {
+    if (code) throw stderr;
+    if (parseFloat(stdout) < 3) {
+      throw new Error('[ERROR: Strapi plugin] You need npm version @>=3');
+      process.exit(1);
+    } else {
+      gitCommit();
+    }
+  });
+};
+
+const gitCommit = () => {
+  console.log('Enter Commit Message:');
+  readFromTerminal(msg => {
+    shell.exec(
+      `git commit -m "${msg}"`,
+      { slient: true },
+      (code, stdout, stderr) => {
+        if (code) throw stderr;
+        if (parseFloat(stdout) < 3) {
+          throw new Error('[ERROR: Strapi plugin] You need npm version @>=3');
+          process.exit(1);
+        } else {
+          gitPush();
+        }
+      }
+    );
+  });
+};
+
+const gitPush = () => {
+  console.log('Enter Branch Name:');
+  readFromTerminal(msg => {
+    shell.exec(
+      `git push origin ${msg.trim()}`,
+      { slient: true },
+      (code, stdout, stderr) => {
+        if (code) throw stderr;
+        if (parseFloat(stdout) < 3) {
+          throw new Error('[ERROR: Strapi plugin] You need npm version @>=3');
+          process.exit(1);
+        }
+      }
+    );
+  });
+};
+
+const init = () => {
+  gitAdd();
+};
+
+init();
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
-const shelljs = require('shelljs');
+const shell = require('shelljs');
 
-shelljs.echo('Hello World');
+console.log(shell.target);
 
-// console.log(shelljs);
+// prints Hello world on terminal
+shell.echo('Hello World');
 
-var a = shelljs.exec('node --version');
+// execute the node version command and print result
+shell.exec('node --version');
 
-console.log(a);
+console.log(shell.pwd().stdout);
+
+// check if git is present or not
+if (!shell.which('git')) {
+  shell.echo('Sorry, this script requires git');
+  shell.exit(1);
+} else {
+  shell.exec('git --version');
+}
+
+console.log(shell.ls());
+
+// to create directory
+shell.mkdir('testing');
+
+shell.cd('testing');
